ccb36685a5d6d20fd240d1961a31d2ee2874057a
Renji-xD <trashmefull@hotmail.com>
2023-01-16 17:16:23 +0100

    update docs

--- docs/_app/immutable/chunks/6-14bd7f28.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/settings/_page.svelte-355f0b4f.js";export{t as component};
---
+++ docs/_app/immutable/chunks/6-20bd8f37.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/settings/_page.svelte-678d93a9.js";export{t as component};
